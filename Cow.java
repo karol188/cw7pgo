4a7643b989f4caa2888c9111e13519ee3d4460b2
s33604 <s33604@pjwstk.edu.pl>
2025-04-17 10:53:58 +0200

    waeawe

---
+++ Cow.java
@@ -0,0 +1,6 @@
+public class Cow extends Animal {
+    @Override
+    public void makeNoise() {
+        System.out.println("Krowa muczy: Muuu!");
+    }
+}
---
+++ Main2.java
@@ -0,0 +1,11 @@
+public class Main2 {
+    public static void main(String[] args) {
+        Animal dog = new Dog();
+        Animal cat = new Cat();
+        Animal cow = new Cow();
+
+        dog.makeNoise();
+        cat.makeNoise();
+        cow.makeNoise();
+    }
+}
